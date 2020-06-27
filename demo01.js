9ccfc95ddaf865503469062ee0cefdc068081acb
hebaotong <963453212@qq.com>
2020-06-27 17:06:42 +0800

    first commint

---
+++ demo01.js
@@ -0,0 +1,15 @@
+/*
+alert('hello js');
+alert('Hello JavaScript');
+document.write('output by js');
+*/
+function test(){
+    alert("1");
+    alert("2");
+}
+test();
+
+var i = 1;
+while(i<10){
+    console.log(i);
+}
